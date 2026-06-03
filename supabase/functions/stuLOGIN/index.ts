03a15ab159934598ad6e4521a1466eed910b385e
IbrahimKhalil-Developer <ibrahim.system.khalil.admin@gmail.com>
2026-06-03 14:06:17 +0300

    Update index.ts

--- supabase/functions/stuLOGIN/index.ts
+++ supabase/functions/stuLOGIN/index.ts
@@ -1,5 +1,6 @@
 import { createClient } from "npm:@supabase/supabase-js@2.49.8";
 
+
 const SUPABASE_URL = Deno.env.get('SUPABASE_URL') ?? '';
 const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? '';
 const SYSTEM_KEY = Deno.env.get('system_key') ?? ''; // ← من Supabase Secrets
